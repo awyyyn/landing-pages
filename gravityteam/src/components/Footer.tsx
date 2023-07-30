c55697aa98c6ffa9dbfa6d673fcaac5eac91ae4d
awyyyn <alwin.puche16@gmail.com>
2023-07-30 10:26:45 +0800

    fixing gaps in footer

--- gravityteam/src/components/Footer.tsx
+++ gravityteam/src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
     return (
         <footer className="lg:px-[160px] pt-[97px] pb-[64px] w-full  ">
-            <div className="flex  justify-between md:max-w-[756px] mx-auto lg:max-w-[1056px] lg:gap-x-[760px] gap-y-[60px] px-[32px]">
+            <div className="flex  justify-between  md:max-w-[756px] mx-auto lg:max-w-[1056px]  gap-y-[60px] px-[32px]">
                 <div className="space-y-[11px] "> 
                     <h2 className="uppercase text-white text-[15px] font-normal not-italic tracking-[0.53px] leading-[24px] w-[116px]">Gravity team</h2>
                     <a href="#about" className="text-[#BCBCBC] block font-normal text-[14px] leading-[22px] not-italic">
@@ -21,8 +21,8 @@ const Footer = () => {
                     className='w-[180px] h-[41.813px]'
                 />
             </div>
-            <div className="flex px-[32px] justify-between  md:px-0 flex-col sm:flex-row md:max-w-[756px] mx-auto gap-y-[20px] mt-[30px] md:mt-[60px] lg:max-w-[1056px] lg:gap-x-[760px]"> 
-                <a href="#" className="text-[#BCBCBC] block font-normal text-[14px] leading-[18.4px] not-italic">
+            <div className="flex px-[32px] justify-between md:px-[32px] flex-col sm:flex-row md:max-w-[756px] lg:max-w-[1056px] mx-auto gap-y-[20px] mt-[30px] md:mt-[60px]"> 
+                <a href="#" className="text-[#BCBCBC] block font-normal text-[14px] leading-[18.4px] not-italic ">
                     Terms of Use & Privacy Policy
                 </a>
                 <span className='text-[#BCBCBC] block font-normal text-[14px] leading-[18.4px] not-italic'>
